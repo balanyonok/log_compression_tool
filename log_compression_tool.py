33b740599c373085a074cc191948c5f24e70038c
Yana Balanenko <balanyonok@gmail.com>
2022-11-13 18:54:09 +0100

    add get_log_files function with tests

---
+++ log_compression_tool.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+Compress the files in log directory to free up some space
+"""
+
+import gzip
+import shutil
+import os
+import sys
+
+
+def main(logs_dir):
+    log_paths = get_log_files(logs_dir)
+    compress_files(log_paths)
+
+
+def get_log_files(logs_dir):
+    """Return a list of files from given directory that aren't compressed yet"""
+    filenames = os.listdir(logs_dir)
+    log_paths = []
+    for filename in filenames:
+        filepath = os.path.join(logs_dir,filename)
+        if os.path.isfile(filepath):
+            # we don't need the .gz or.xz files
+            if not filename.endswith(".gz"):
+               log_path = os.path.join(logs_dir, filename)
+               log_paths.append(log_path)
+    return log_paths
+
+
+def compress_files(log_paths):
+    """
+    Compress files in given list using gzip module.
+
+    Original file is not removed, compressed copy is saved in the same directory
+    with ".gz" suffix.
+    For example, the directory will contain both `file.log` and `file.log.gz`.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    logs_dir = sys.argv[0]
+    main(logs_dir)
---
+++ test_get_log_files.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+
+"""
+Test the get_log_files() function.
+The result should be a list of non-compressed files in given directory.
+"""
+
+import pytest
+from log_compression_tool import get_log_files
+import os
+
+
+def test_dir_with_logfiles():
+    """
+    Testcase: there are only log files in given directory.
+    """
+    logs_dir = "tests/data/basic"
+    expected_log_paths = [
+        "tests/data/basic/testlog3",
+        "tests/data/basic/testfile2.log",
+        "tests/data/basic/testfile1.log",
+
+    ]
+    assert get_log_files(logs_dir) == expected_log_paths
+
+
+def test_dir_with_zipped_files_too():
+    """
+    Testcase: there are .log files AND .gz files as well
+    """
+    logs_dir = "tests/data/basic_and_zipped"
+    expected_log_paths = [
+        "tests/data/basic_and_zipped/testfile2.log",
+        "tests/data/basic_and_zipped/testfile1.log",
+    ]
+
+    assert get_log_files(logs_dir) == expected_log_paths
+
+
+def test_dir_with_diff_zipped_files():
+    """
+    Testcase: there are also compressed files with different extension
+    in the given directory.
+    """
+    logs_dir = "tests/data/diff_zipped"
+    expected_log_paths = [
+        "tests/data/diff_zipped/testfile1.log",
+    ]
+
+    assert get_log_files(logs_dir) == expected_log_paths
+
+
+def test_dir_only_with_zipped_files():
+    """
+    Testcase: there are ONLY already compressed files in given directory.
+    """
+    logs_dir = "tests/data/zipped"
+    expected_log_paths = []
+
+    assert get_log_files(logs_dir) == expected_log_paths
+
+
+def test_dir_with_empty_subdirs():
+    """
+    Testcase: there are empty subdirectories in given directory.
+    """
+    logs_dir = "tests/data/empty_subdir"
+    expected_log_paths = [
+        "tests/data/empty_subdir/testlog3",
+        "tests/data/empty_subdir/testfile2.log",
+        "tests/data/empty_subdir/testfile1.log",
+
+    ]
+
+    assert get_log_files(logs_dir) == expected_log_paths
+
+
+def test_dir_with_subdirs():
+    """
+    Testcase: there are subdirectories with log files in given directory.
+    """
+    logs_dir = "tests/data/tricky_dir"
+    expected_log_paths = [
+        "tests/data/tricky_subdir/testlog3",
+        "tests/data/tricky_subdir/testfile2.log",
+        "tests/data/tricky_subdir/testfile1.log",
+    ]
+
+    assert get_log_files(logs_dir) == expected_log_paths
